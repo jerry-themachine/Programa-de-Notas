7edf20f68455ae7da25b160252a7d375e99e599a
Jerry William <wilguepardo@gmail.com>
2022-02-17 17:51:30 -0300

    Projeto Programa de Notas - 5ª Modificação (Elaborando estilização e  validação para formulário da aplicação)

--- listaDetarefas.js
+++ listaDetarefas.js
@@ -1,22 +1,56 @@
+//Criando novas listas, inseridno e riscando a palavra após clicar no botão concluir
+(() => { let criarTarefa = ((evento) => { //Criando evento de click para inserir nova lista
+        var novoInput = document.getElementById("input");
 
-//Selecionando elemento pelo data atribut
-let novaTarefa = document.querySelector('[data-form-button]');
-let lista = document.querySelector('[data-list]');
+        novoInput.oninvalid = function () {
+            this.setCustomValidity('');
+            if(!this.validity.valid){
+                this.setCustomValidity(`Preencha o campo para continuar`);               
+            } else {   
+                    this.setCustomValidity(` `);              
+                    
+                evento.preventDefault();
 
+                //let input = document.querySelector('[data-form-input]');
+                let valor = this.value;
+                let lista = document.querySelector('[data-list]');
+                let tarefa = document.createElement('li'); // Criando um novo item dentro de DOM através de Javascript
 
-let criarTarefa = ((evento) => { //Criando evento de click para inserir nova lista
+                
+                    
+                tarefa.classList.add('item')// Adicionando a class 'item' ao elemento 'li'
+                let conteudo = `<p class="content">${valor}</p>`
+                tarefa.innerHTML = conteudo;
+                lista.appendChild(tarefa); //Inserindo o elemento 'li' no final do nó, dentro do elemento 'ul'
+                tarefa.appendChild(BotaoConclui());
+                this.value = ''; //Limpando o elemento 'input' do formulário'
+            }
+        };
+    
+    });
 
-    evento.preventDefault();
+    //Selecionando um elemento pelo data atribut
+    let novaTarefa = document.querySelector('[data-form-button]');
+    novaTarefa.addEventListener('click', criarTarefa);//Invocando o evento com a function 'criarTerefa'
 
-    let input = document.querySelector('[data-form-input]');
-    let valor = input.value;
-    let novaLista = document.createElement('li'); // Criando um novo item dentro de DOM através de Javascript
+    let BotaoConclui = () => {
 
-    novaLista.classList.add('item')// Adicionando a class 'item' ao elemento 'li'
-    novaLista.textContent = valor;
-    lista.appendChild(novaLista); //Inserindo o elemento 'li' no final do nó, dentro oa elemento 'ul'
-    input.value = ''; //Limpando o elemento 'input' do formulário'
-});
+        let botaoConclui = document.createElement('button');
 
-novaTarefa.addEventListener('click', criarTarefa);//Invocando o evento com a function 'criarTerefa'
+        botaoConclui.classList.add('check-button');// Adicionando a class 'check-button' ao elemento 'button'
+        botaoConclui.innerText = 'Concluir';
+        botaoConclui.addEventListener('click', concluirTarefa);    
 
+        return botaoConclui;    
+    };
+
+    //Criando a função para riscar a tarefa concluída
+    let concluirTarefa = (evento) => {
+
+        let botaoConcluir = evento.target;// Criando variável para descobrir o elemento clicado través do 'target'
+        let tarefCompleta = botaoConcluir.parentElement;// Pegando o pai do botão, o elemento 'li', utilizando  'parentElement'
+
+        //Executando a classe 'done' no CSS a partir do momento de click do botão, através do método 'toggle'
+        tarefCompleta.classList.toggle('done');//Toggle deveolve um booleano (verdaeiro/clicado ou falso/não-clicado) 
+    };
+})();
